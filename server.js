25204122a8370d3615ecfd5b49107b1564177119
Be Lee <StudioBeLee@gmail.com>
2016-04-26 20:54:51 -0700

    add project page

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
  var express = require('express');
  var app = express();
+ var exphbs = require('express-handlebars');
  var port = process.env.PORT || 3000;
 
+
+ // middleware 
+
+app.set('views', 'views');
+
+app.engine('hbs', exphbs({
+	extname:'hbs',
+	defaultLayout: 'main',
+	layoutsDir:'./views/layouts'
+}));
+
+
+
+app.set('view engine','hbs');
+
+app.use(express.static('public'));
+
+//routes
+
+
 app.get('/',function(request, response){
-	response.send('Hi There!')
-})
+	response.render('home');
+});
+
+app.get('/projects', function(request, response){
+ response.render('projects');
+});
+
+//server 
+
  app.listen(port, function(){
  	console.log('Servers is running on ' + port);
 
