e7397782e48360ece43155158b7c89f648d15253
Be Lee <StudioBeLee@gmail.com>
2016-04-26 19:51:30 -0700

    And home route

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
- 
+ var express = require('express');
+ var app = express();
+ var port = process.env.PORT || 3000;
+
+app.get('/',function(request, response){
+	response.send('Hi There!')
+})
+ app.listen(port, function(){
+ 	console.log('Servers is running on ' + port);
+
+ })
+
